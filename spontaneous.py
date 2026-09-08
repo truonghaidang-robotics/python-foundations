4587368c088878957091331e755a615f23bb7804
truonghaidang-robotics <truonghaidang53@gmail.com>
2026-09-08 19:18:15 +0800

    set

---
+++ spontaneous.py
@@ -0,0 +1,13 @@
+set1 = {23, 56, 78, 21, 56}
+print(set1)
+21 in set1
+len(set1)
+print(21 in set1)
+print(len(set1))
+print(type (set1))
+set2 = set('anchefihe')
+print(set2)
+set3 = set('hjuisjfw')
+print(set3-set2)
+print(set2&set3)
+print(set2|set3)
