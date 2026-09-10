0ff63520bcec19f96751c9f856556ce097c8e71f
truonghaidang-robotics <truonghaidang53@gmail.com>
2026-09-10 16:16:31 +0800

    set+dict

--- spontaneous.py
+++ spontaneous.py
@@ -11,3 +11,16 @@ set3 = set('hjuisjfw')
 print(set3-set2)
 print(set2&set3)
 print(set2|set3)
+
+date = {'haidang':34, 'john':23, 'jane':45, 'bob':57}
+print(date['haidang'])
+print(date.get('john'))
+keys = ['navin', 'histesh', 'hdang']
+values = [34, 64,87]
+dict1 = dict(zip(keys,values))
+print(dict1)
+date = {'js':'vscode', 'python': ['vscode', 'pychamr'], 'java': {'core': 'vscode', 'spring': 'iij'}}
+print(date)
+print(date['js'])
+print(date['python'][1])
+print(date['java']['core'])
